b00dc9b8ab57ce60cb0f4a15888389ee327f5663
Faguilar <goguisalf@gmail.com>
2020-02-06 22:14:00 -0600

    Modificacion de Boton de Exportacion

--- ComplemetoBack/Export.py
+++ ComplemetoBack/Export.py
@@ -1,5 +1,6 @@
 import sqlite3
 import datetime
+import time
 from sqlite3 import Error
 from flask import Flask, request, render_template
 from flask_restful import Resource, Api, reqparse
@@ -7,37 +8,38 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators,
 from xlsxwriter.workbook import Workbook
 
 
-def export():
-    if request.method == 'POST':
-        if 'dir_target' in request.values:
-            file2_ = str(request.form['dir_target'])
-            hnd = str(datetime.date.today())
-            namebook = 'port'
-            print(file2_)
-            dirtarget2 = file2_ + "\Ex"
-            xbook = dirtarget2 + namebook + hnd + '.xlsx'
-            workbook = Workbook(xbook)
-            worksheet = workbook.add_worksheet()
-            bold = workbook.add_format({'bold': True, 'font_color': 'blue'})
-            conn = sqlite3.connect('zapiensa_project_v2.db')
-            c = conn.cursor()
-            c.execute("SELECT * FROM view_receivers")
-            mysel = c.execute("SELECT * FROM view_receivers")
-            col_name_list = [tuple[0] for tuple in mysel.description]
-            row = 0
-            col = 0
-            tp = 0
+def export(filename):
+    xfile = None
+    file2_ = '.\\uploads\\'
+    namebook = 'port'
+    print(file2_)
+    dirtarget2 = file2_ + "Ex"
+    xbook = dirtarget2 + namebook + filename + '.xlsx'
+    workbook = Workbook(xbook)
+    worksheet = workbook.add_worksheet()
+    bold = workbook.add_format({'bold': True, 'font_color': 'blue'})
+    conn = sqlite3.connect('zapiensa_project_v2.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM view_receivers")
+    mysel = c.execute("SELECT * FROM view_receivers")
+    col_name_list = [tuple[0] for tuple in mysel.description]
+    row = 0
+    col = 0
+    tp = 0
 
-            for i in col_name_list:
-                worksheet.write(row, col, col_name_list[tp], bold)
-                col = col + 1
-                tp = tp + 1
+    for i in col_name_list:
+        worksheet.write(row, col, col_name_list[tp], bold)
+        col = col + 1
+        tp = tp + 1
 
-            rows = mysel.fetchall()
+    rows = mysel.fetchall()
 
-            for r, row in enumerate(rows):
-                for c, col in enumerate(row):
-                    worksheet.write(r + 1, c, col)
+    for r, row in enumerate(rows):
+        for c, col in enumerate(row):
+            worksheet.write(r + 1, c, col)
 
-            workbook.close()
-            conn.close()
+    workbook.close()
+    conn.close()
+    xfile = xbook
+    print(xfile)
+    return xfile
--- ComplemetoBack/ex_db.py
+++ ComplemetoBack/ex_db.py
@@ -1,8 +1,8 @@
-import os
+import os, time
 from os import remove
 from xls2db import xls2db
 import sqlite3
-from flask import Flask, request, app, render_template, flash
+from flask import Flask, request, app, render_template, flash, send_from_directory, send_file
 from Export import export
 from Respaldo import respaldo
 
@@ -138,16 +138,21 @@ def importation():
 
     return render_template('SU Exportaciones.html', una_lista=['Tipo ZAP Academy', 'Tipo Apoyo a Mujeres',
                                                                'Tipo Jalisco te Reconoce', 'Otro Tipo'],
-                           backup_file=backup_file)
+                           backup_file=backup_file, now=time.strftime("%Y%m%d-%H%M%S"))
 
 
 @app.route("/exports", methods=['GET', 'POST'])
 def exports():
-    export()
-    return render_template('SU Exportaciones.html', my_list=('Tipo ZAP Academy',
-                                                             'Tipo Apoyo a Mujeres',
-                                                             'Tipo Jalisco te Reconoce',
-                                                             'Otro Tipo'))
+
+    exportfile = export(request.args['filename'])
+    print(exportfile)
+    return send_file(exportfile, as_attachment=True)
+
+
+@app.route("/backup", methods=['GET', 'POST'])
+def backup():
+    file = respaldo()
+    return send_file('zapiensa_project_v2.db', attachment_filename='zapiensa_project_v2' + time.strftime("-%Y%m%d-%H%M%S") + '.db', as_attachment=True)
 
 
 if __name__ == '__main__':
